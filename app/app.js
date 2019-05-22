3bcfe2c10584afb86f10d40884aa7a2479eeb1ea
peppy-shubhi <shubhikhandelwal98@gmail.com>
2019-05-22 11:28:27 +0530

    ng-show ng-hide

--- app/app.js
+++ app/app.js
@@ -6,12 +6,15 @@ myApp.controller('myController',['$scope',function($scope){
     $scope.arr=[
         {name:"shubhi",
         rate:"20",
+        available:true
     },
     {name:"nidhi",
         rate:"25",
+        available:true
     },
     {name:"yash",
         rate:"17",
+        available:false
     },
     ];
 
